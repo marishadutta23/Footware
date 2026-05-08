940e6fd07631ed5f2dd4ac3756cc2e16accfc474
marisha <marishadutta23@gmail.com>
2026-05-08 14:41:18 +0530

    fixed again

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 
-export const API_BASE_URL = "${import.meta.env.VITE_API_URL}/api";
+export const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const API_ENDPOINTS = {
   CHECKOUT_ORDER: "orders/checkout", // ✅ FIXED
